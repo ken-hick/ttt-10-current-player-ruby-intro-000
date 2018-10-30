becc0921b63f53062e072246d4faf1826ffb91f3
Ken Hick <hick.ken@gmail.com>
2018-10-30 16:11:40 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 # turn_count method
-# checks to see if valid input of X or O 
+# checks to see if valid input of X or O
 # increases turn count by 1
 
 def turn_count("X", "O", "X", "O", "X", "O", "X", "O", "X")
