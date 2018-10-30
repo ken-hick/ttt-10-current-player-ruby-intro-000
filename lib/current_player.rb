ef61bac514d59434bfe1aeb9b69b0c14d0d33c5f
Ken Hick <hick.ken@gmail.com>
2018-10-30 16:12:58 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@
 # checks to see if valid input of X or O
 # increases turn count by 1
 
-def turn_count ("X", "O", "X", "O", "X", "O", "X", "O", "X")
+def turn_count("X", "O", "X", "O", "X", "O", "X", "O", "X")
   counter = 0
   turn_count.each do |turn|
     if turn == "X" || "O"
