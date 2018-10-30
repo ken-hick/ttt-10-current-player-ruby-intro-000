46d9242fb1b4c5b0b1fa70f9f9d56b32a02047a8
Ken Hick <hick.ken@gmail.com>
2018-10-30 16:13:19 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,9 +3,5 @@
 # increases turn count by 1
 
 def turn_count("X", "O", "X", "O", "X", "O", "X", "O", "X")
-  counter = 0
-  turn_count.each do |turn|
-    if turn == "X" || "O"
-      counter += 1
-    end
+
   end
